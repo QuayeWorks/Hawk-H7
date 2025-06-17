847b162f2210c2b38573f8fff9b28bf70ff6d5a5
QuayeWorks <113059255+QuayeWorks@users.noreply.github.com>
2025-06-17 16:00:04 -0400

    Add files via upload

--- Core/Inc/debug_menu.h
+++ Core/Inc/debug_menu.h
@@ -14,4 +14,15 @@ void DebugMenu_Init(UART_HandleTypeDef *huart);
 void DebugMenu_Task(void);
 void DebugMenu_ForceInput(uint8_t ch);
 
+typedef enum {
+    DEBUG_MENU_NONE    = 0x00,
+    DEBUG_MENU_SENSORS = 0x01,
+    DEBUG_MENU_PPM     = 0x02,
+    DEBUG_MENU_BUZZER  = 0x04,
+    DEBUG_MENU_HELP    = 0x08
+} DebugMenu_Mask;
+
+void DebugMenu_SetActionMask(uint8_t mask);
+uint8_t DebugMenu_GetActionMask(void);
+
 #endif // DEBUG_MENU_H
